402e4703f157ed8a0a4abd222a98284faa1e1880
yrueg <yaron.rueger@gmx.de>
2023-03-07 12:29:12 +0100

    SQL change

--- Postgres/wab3SQL.sql
+++ Postgres/wab3SQL.sql
@@ -1,10 +1,43 @@
-DROP TABLE IF EXISTS tkey_schirmbox;
 DROP TABLE IF EXISTS tkey_device;
+DROP TABLE IF EXISTS tkey_schirmbox;
 DROP TABLE IF EXISTS tkey_usbHub;
 DROP TABLE IF EXISTS tkey_server;
 DROP TABLE IF EXISTS tkey_rack;
 DROP TABLE IF EXISTS tkey_location;
 
+CREATE TABLE tkey_location(
+        id_city varChar(50) PRIMARY KEY,
+        ds_country varChar(50),
+        ds_building varChar(50)
+);
+
+CREATE TABLE tkey_rack(
+        id_number varChar(50) PRIMARY KEY,
+        fi_location varChar(50),
+        FOREIGN KEY (fi_location) REFERENCES tkey_location(id_city)
+);
+
+
+CREATE TABLE tkey_server(
+        id_ipAddress varChar(50) PRIMARY KEY,
+        ds_labOrTest varChar(50),
+        fi_rackNumber varChar(50),
+        FOREIGN KEY (fi_rackNumber) REFERENCES tkey_rack(id_number)
+);
+
+CREATE TABLE tkey_usbHub(
+        id_id varChar(50) PRIMARY KEY,
+        ds_btMac varChar(50),
+        fi_rackNumber varChar(50),
+        FOREIGN KEY (fi_rackNumber) REFERENCES tkey_rack(id_number)
+);
+
+CREATE TABLE tkey_schirmbox(
+        id_schirmboxName varChar(50) PRIMARY KEY,
+        ds_btMac varChar(50),
+        fi_rackNumber varChar(50),
+        FOREIGN KEY (fi_rackNumber) REFERENCES tkey_rack(id_number)
+);
 
 CREATE TABLE tkey_device(
         id_androidID varChar(50) PRIMARY KEY,
@@ -13,33 +46,9 @@ CREATE TABLE tkey_device(
         ds_imei varChar(50),
         ds_btMac varChar(50),
         dy_batteryProj boolean,
-        dn_usbPort int
-);
-
-CREATE TABLE tkey_schirmbox(
-        id_name varChar(50) PRIMARY KEY,
-        ds_btMac varChar(50),
-        fi_androidID varChar(50),
-        FOREIGN KEY (fi_androidID) REFERENCES tkey_device(id_androidID)
-);
-
-
-CREATE TABLE tkey_usbHub(
-        id_id varChar(50) PRIMARY KEY,
-        ds_btMac varChar(50)
-);
-
-CREATE TABLE tkey_server(
-        id_ipAdress varChar(50) PRIMARY KEY,
-        ds_labOrTest varChar(50)
-);
-
-CREATE TABLE tkey_rack(
-        id_nummer varChar(50) PRIMARY KEY
-);
-
-CREATE TABLE tkey_location(
-        id_city varChar(50) PRIMARY KEY,
-        ds_country varChar(50),
-        ds_building varChar(50)
+        dn_usbPort int,
+        fi_schirmboxName varChar(50),
+        fi_ipAddress varChar(50),
+        FOREIGN KEY (fi_ipAddress) REFERENCES tkey_server(id_ipAddress),
+        FOREIGN KEY (fi_schirmboxName) REFERENCES tkey_schirmbox(id_schirmboxName)
 );
