de0336c358df10a5f43a6f2d2f2325d065139052
yrueg <yaron.rueger@gmx.de>
2023-03-14 13:23:46 +0100

    added SQL data

--- Postgres/wab3SQL.sql
+++ Postgres/wab3SQL.sql
@@ -56,10 +56,37 @@ CREATE TABLE tkey_device(
         fi_schirmboxName text REFERENCES tkey_schirmbox(id_schirmboxName)
 );
 
-INSERT INTO tkey_location VALUES ('Nuernberg', 'Germany', 'GeorgElserStraße2');
-INSERT INTO tkey_rack VALUES ('1', 'Nuernberg');
-INSERT INTO tkey_usbHub VALUES ('101213', '2fa3d', '1');
-INSERT INTO tkey_server VALUES ('10.112.125.01', 'my-nyge-sm-1', 'lab', '1');
-INSERT INTO tkey_schirmbox VALUES ('schirmbox1', '12fa34b', '1');
-INSERT INTO tkey_heRack VALUES (1, 0, '10.112.125.01', 'schirmbox1', '1');
-INSERT INTO tkey_device VALUES ('12hf1', 'Samsung21', '121212', '22323', 'btmac1', true, 1, '10.112.125.01', 'schirmbox1');
+INSERT INTO tkey_location(id_city, ds_country, ds_building) VALUES
+('city1', 'country1', 'building1'),
+('city2', 'country2', 'building2'),
+('city3', 'country3', 'building3');
+
+INSERT INTO tkey_rack(id_number, fi_location) VALUES
+('number1', 'city1'),
+('number2', 'city2'),
+('number3', 'city3');
+
+INSERT INTO tkey_usbHub(id_id, ds_btMac, fi_rackNumber) VALUES
+('id1', 'btmac1', 'number1'),
+('id2', 'btmac2', 'number2'),
+('id3', 'btmac3', 'number3');
+
+INSERT INTO tkey_server(id_ipAddress, ds_name, ds_labOrTest, fi_rackNumber) VALUES
+('ip1', 'name1', 'lab', 'number1'),
+('ip2', 'name2', 'test', 'number2'),
+('ip3', 'name3', 'lab', 'number3');
+
+INSERT INTO tkey_schirmbox(id_schirmboxName, ds_btMac, fi_rackNumber) VALUES
+('schirmbox1', 'btmac1', 'number1'),
+('schirmbox2', 'btmac2', 'number2'),
+('schirmbox3', 'btmac3', 'number3');
+
+INSERT INTO tkey_heRack(dn_he, fi_ipAddress, fi_schirmboxName, fi_rack) VALUES
+(1, 'ip1', 'schirmbox1', 'number1'),
+(2, 'ip2', 'schirmbox2', 'number2'),
+(3, 'ip3', 'schirmbox3', 'number3');
+
+INSERT INTO tkey_device(id_androidID, ds_model, ds_msisdn, ds_imei, ds_btMac, dy_batteryProj, dn_usbPort, fi_ipAddress, fi_schirmboxName) VALUES
+('androidID1', 'model1', 'msisdn1', 'imei1', 'btmac1', true, 1, 'ip1', 'schirmbox1'),
+('androidID2', 'model2', 'msisdn2', 'imei2', 'btmac2', false, 2, 'ip2', 'schirmbox2'),
+('androidID3', 'model3', 'msisdn3', 'imei3', 'btmac3', true, 3, 'ip3', 'schirmbox3');
