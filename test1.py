0c6d3d2653e31af4ac5046557126797ec08dc0ef
yrueg <yaron.rueger@gmx.de>
2023-03-18 18:00:27 +0100

    added GUI functions

--- test1.py
+++ test1.py
@@ -7,12 +7,17 @@ _maintainer_ = "Yaron Rueger"
 ########################################################################
 #TODO:
 #   [0] add button to save changes
+#   [x] get the tableNames from the database
+#   [x] name of the table
 #   [] change button to add the table not replace it
-#   [] change size of table
-#   [] darkmode
+#   [x] change size of table
+#       -->[]place them one in a row
+#   [x] darkmode
+#   [x] get the columnsnames from the database
 #   [] scrollbar for window
-#   [] name of the table
+#   [] add real data
 ########################################################################
+
 import tkinter as tk
 from tkinter import *
 import psycopg2
@@ -23,12 +28,12 @@ window = tk.Tk()
 for i in range(10):
     window.grid_columnconfigure(i, weight=1)
 
+#space between buttons and tables
+window.grid_rowconfigure(6, minsize=30)
 #window.grid_rowconfigure(2, weight=1)    
 window.title("WAB3_DB")
 window.config(padx=50, pady=50)
 window.config(bg="#2d2d2d")
-
-
 window.geometry("1000x500")
 #scrollbar = Scrollbar(window)
 #scrollbar.pack( side = RIGHT, fill = Y )
@@ -37,27 +42,71 @@ window.geometry("1000x500")
 conn = psycopg2.connect(database="wab3_db", user="postgres",password="R00t1", host="localhost", port="5432")
 cur = conn.cursor()
 
-tables=["tkey_location", "tkey_rack", "tkey_usbHub", "tkey_server", "tkey_schirmbox", "tkey_heRack", "tkey_device"]
+#creates list of table names
+tables=[]
+cur.execute("""SELECT table_name FROM information_schema.tables
+       WHERE table_schema = 'public'""")
+tableNames = cur.fetchall()
+tablesLength = len(tableNames)
+for i in range(tablesLength):
+    tables.append(tableNames[i][0])
+
+
 sheets = []
 buttons = []
+tkeyName = "null"
 
-def get_tables():
-    delete_tables()
-    j = 0
-    for i in tables:
-        cur.execute("SELECT * FROM " + i)
-        data = cur.fetchall()
-        newdata = []
-        for i in data:
-            newdata.append(list(i))
-        sheets.append(tksheet.Sheet(window, data=newdata))
-        sheets[-1].grid(row=math.floor(j/2)+6, column=j%2)
-        sheets[-1].enable_bindings(("single_select", "row_select", "column_width_resize","arrowkeys","right_click_popup_menu","rc_select", "rc_insert_row","rc_delete_row", "copy", "cut", "paste", "delete","undo","edit_cell"))
-        j+=1
+#######################get_allTables()#########################################
+#def get_tables():
+#    delete_tables()
+#    j = 0
+#    for i in tables:
+#        cur.execute("SELECT * FROM " + i)
+#        data = cur.fetchall()
+#        newdata = []
+#        for i in data:
+#            newdata.append(list(i))
+#        sheets.append(tksheet.Sheet(window, data=newdata))
+#        sheets[j].grid(row=j+6, column=0, rowspan=20, columnspan=9, sticky="nsew")
+#        sheets[j].enable_bindings(("single_select", "row_select", "column_width_resize","arrowkeys","right_click_popup_menu","rc_select", "rc_insert_row","rc_delete_row", "copy", "cut", "paste", "delete","undo","edit_cell"))
+#        sheets[j].extra_bindings([("end_edit_cell", cell_edited)])
+#        j+=1
 
+#######################cell_edited()#########################################
+#def cell_edited(event):
+#    changedData = event[3]
+#    row = event[0]
+#    column = event[1]
+
+def cell_editedOne(event):
+    print(event)
+    changedData = event[3]
+    print(changedData)
+    columnsName = sheets[-1].get_sheet_data(return_copy = False, get_header = True, get_index = False)[event[1]]
+    print(columnsName)
+    idName = sheets[-1].get_sheet_data(return_copy = False, get_header = True, get_index = False)[0]
+    print(idName)
+    idWert = sheets[-1].get_cell_data(event[0], 0, return_copy = True)
+    print(idWert)
+    cur.execute("UPDATE " + tkeyName + " SET " + columnsName + " = " + "'"+changedData +"'"+ " WHERE " + idName + " = " + "'"+idWert+"'")
+
+label1= None
 def get_oneTable(name):
+    columns=[]
     global sheets
+    global tkeyName
+    tkeyName = name
+    global label1
     delete_tables()
+    
+    label1 = tk.Label(window, text=name)
+    label1.grid(row=7, column=0, sticky="nsew")
+
+    cur.execute("Select * FROM "+ name +" LIMIT 0")
+    columnLength= cur.description
+    for i in range(len(columnLength)):
+        columns.append(columnLength[i][0])
+
     cur.execute("SELECT * FROM " + name)
     data = cur.fetchall()
     newdata = []
@@ -65,26 +114,30 @@ def get_oneTable(name):
         newdata.append(list(i))
     sheets.append(tksheet.Sheet(window, data=newdata,enable_edit_cell_auto_resize = True, auto_resize_default_row_index = True))
     sheets[-1].enable_bindings(("single_select", "row_select", "column_width_resize","arrowkeys","right_click_popup_menu","rc_select", "rc_insert_row","rc_delete_row", "copy", "cut", "paste", "delete","undo","edit_cell"))
-    sheets[-1].grid(row=7,column=0, rowspan=20, columnspan=9, sticky="nsew")
-    sheets[-1].headers(["Name", "Type", "Value", "Unit", "Min", "Max", "Default"])
+    sheets[-1].grid(row=8,column=0, rowspan=20, columnspan=9, sticky="nsew")
+    sheets[-1].headers(columns)
+    sheets[-1].extra_bindings([("end_edit_cell", cell_editedOne)])
+    
 
 def delete_tables():
+    global label1
     for i in reversed(sheets):
         i.grid_forget()
         sheets.remove(i)
+        label1.grid_forget()
 
-button1 = tk.Button(text="show all", bg="#262626", fg="white", font=("Arial", 12), command=get_tables)
-button1.grid(row=0,column=0, sticky= "nsew")
+#button1 = tk.Button(text="show all", bg="#262626", fg="white", font=("Arial", 12), command=get_tables)
+#button1.grid(row=0,column=0, sticky= "nsew")
 
 button2 = tk.Button(text="show nothing", bg="#262626", fg="white", font=("Arial", 12), command=delete_tables)
-button2.grid(row=0,column=1, sticky= "nsew")
+button2.grid(row=3,column=1, sticky= "nsew")
 
 for i in tables:
     button = tk.Button(text=i, bg="#262626", fg="white", font=("Arial", 12), command=lambda n=i: get_oneTable(n))
     buttons.append(button)
 
 for i in range(len(buttons)):
-    buttons[i].grid(row=math.floor(i/2)+2,column=i%2, sticky= "nsew")
+    buttons[i].grid(row=math.floor(i/2),column=i%2, sticky= "nsew")
 window.mainloop()
 
 def save_data():
--- test5.py
+++ test5.py
@@ -15,6 +15,22 @@ conn = psycopg2.connect(database="wab3_db", user="postgres", password="R00t1", h
 cur = conn.cursor()
 
 
+cur.execute("""SELECT table_name FROM information_schema.tables
+       WHERE table_schema = 'public'""")
+tables = cur.fetchall()
+for table in tables:
+    print(table)
+zahl = len(tables)
+
+cur.execute("Select * FROM "+ tables[0][0] +" LIMIT 0")
+print(cur.description)
+columns = cur.description
+for i in range(len(columns)):
+    print(columns[i][0])
+#columns = cur.fetchall()
+#print(columns)
+#print(len(tables))
+#print(tables[zahl-1][0])
 
 # Retrieve the data for the table from the database
 cur.execute(f"SELECT * FROM tkey_location")
@@ -23,16 +39,26 @@ newdata = []
 for i in data:
     newdata.append(list(i))
 
-print(newdata)
-
+#print(newdata)
+def cell_edited(event):
+    #print(event[3])
+    ...
+columnsList = []
+for i in range(len(columns)):
+    columnsList.append(columns[i][0])
 sheet = tksheet.Sheet(window, data=newdata)
 sheet.grid(row=0, column=0)
 sheet.enable_bindings(("single_select", "row_select", "column_width_resize","arrowkeys","right_click_popup_menu","rc_select", "rc_insert_row","rc_delete_row", "copy", "cut", "paste", "delete","undo","edit_cell"))
+sheet.extra_bindings([("end_edit_cell", cell_edited)])
+sheet.headers(columnsList)
 
-print(sheet.get_sheet_data())
-newdata = sheet.get_sheet_data()
 
-cur.execute("INSERT INTO tkey_location VALUES " + newdata[0][0])
+
+
+#print(sheet.get_sheet_data())
+#newdata = sheet.get_sheet_data()
+
+#cur.execute("INSERT INTO tkey_location VALUES " + newdata[0][0])
 # Close the database connection when the program exits
 
 window.mainloop()
