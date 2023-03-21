7ef5e53b50fa97fa3c55d02aed60d414ba79ee2f
yaronrueger <yaron.rueger@gmx.de>
2023-03-21 10:06:10 +0100

    new Table

--- Postgres/wab3SQL.sql
+++ Postgres/wab3SQL.sql
@@ -31,7 +31,6 @@ CREATE TABLE tkey_server(
 
 CREATE TABLE tkey_schirmbox(
         id_schirmboxName TEXT PRIMARY KEY,
-        ds_btMac TEXT,
         fi_rackNumber TEXT REFERENCES tkey_rack(id_number)
 );
 
@@ -43,6 +42,12 @@ CREATE TABLE tkey_heRack(
         fi_rack TEXT REFERENCES tkey_rack(id_number)
 );
 
+CREATE TABLE tkey_bluetoothDongle(
+        id_btmac TEXT PRIMARY KEY,
+        ds_usbPort TEXT PRIMARY KEY,
+        fi_schirmboxName TEXT REFERENCES tkey_schirmbox(id_schirmboxName)
+);
+
 CREATE TABLE tkey_device(
         id_androidID TEXT PRIMARY KEY,
         ds_model TEXT,
@@ -51,6 +56,7 @@ CREATE TABLE tkey_device(
         ds_btMac TEXT,
         dy_batteryProj TEXT,
         dn_usbPort TEXT,
+        fi_dongle TEXT REFERENCES tkey_bluetoothDongle(id_btmac),
         fi_ipAddress TEXT REFERENCES tkey_server(id_ipAddress),
         fi_schirmboxName TEXT REFERENCES tkey_schirmbox(id_schirmboxName)
 );
@@ -75,19 +81,24 @@ INSERT INTO tkey_server(id_ipAddress, ds_name, ds_labOrTest, fi_rackNumber) VALU
 ('ip2', 'name2', 'test', 'number2'),
 ('ip3', 'name3', 'lab', 'number3');
 
-INSERT INTO tkey_schirmbox(id_schirmboxName, ds_btMac, fi_rackNumber) VALUES
-('schirmbox1', 'btmac1', 'number1'),
-('schirmbox2', 'btmac2', 'number2'),
-('schirmbox3', 'btmac3', 'number3');
+INSERT INTO tkey_schirmbox(id_schirmboxName, fi_rackNumber) VALUES
+('schirmbox1', 'number1'),
+('schirmbox2', 'number2'),
+('schirmbox3', 'number3');
 
 INSERT INTO tkey_heRack(id_id,dn_he, fi_ipAddress, fi_schirmboxName, fi_rack) VALUES
 ('1','1', 'ip1', 'schirmbox1', 'number1'),
 ('2','2', 'ip2', 'schirmbox2', 'number2'),
 ('3','3', 'ip3', 'schirmbox3', 'number3');
 
-INSERT INTO tkey_device(id_androidID, ds_model, ds_msisdn, ds_imei, ds_btMac, dy_batteryProj, dn_usbPort, fi_ipAddress, fi_schirmboxName) VALUES
-('androidID1', 'model1', 'msisdn1', 'imei1', 'btmac1', 'true', '1', 'ip1', 'schirmbox1'),
-('androidID2', 'model2', 'msisdn2', 'imei2', 'btmac2', 'false', '2', 'ip2', 'schirmbox2'),
-('androidID3', 'model3', 'msisdn3', 'imei3', 'btmac3', 'true', '3', 'ip3', 'schirmbox3');
+INSERT INTO tkey_bluetoothDongle VALUES 
+('1','2','schirmbox1'),
+('1','4','schirmbox1'),
+('1','6','schirmbox1');
+
+INSERT INTO tkey_device(id_androidID, ds_model, ds_msisdn, ds_imei, ds_btMac, dy_batteryProj, dn_usbPort, fi_dongle, fi_ipAddress, fi_schirmboxName) VALUES
+('androidID1', 'model1', 'msisdn1', 'imei1', 'btmac1', 'true', '1','1', 'ip1', 'schirmbox1'),
+('androidID2', 'model2', 'msisdn2', 'imei2', 'btmac2', 'false', '2','1', 'ip2', 'schirmbox2'),
+('androidID3', 'model3', 'msisdn3', 'imei3', 'btmac3', 'true', '3','1', 'ip3', 'schirmbox3');
 
 --SELECT * FROM tkey_location INNER JOIN tkey_rack ON tkey_location.id_city=tkey_rack.fi_location;
